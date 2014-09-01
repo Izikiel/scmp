c953dc00d6286fbbcf5291996f07b9e0a039fa0c
Ezequiel Dario Gambaccini <ezequiel.gambaccini@gmail.com>
2014-08-31 23:27:16 -0300

    Agrego makefile, updateo gitignore

--- tp1/explicit.c
+++ tp1/explicit.c
@@ -1,5 +1,6 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 double solve_explicit(double next, double actual, double prev);
 double solve_implicit(double new_next, double new_prev, double next, double actual, double prev);
