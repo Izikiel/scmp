1efa9af92ca32d681000663bbbf067979f12accc
Ezequiel Dario Gambaccini <ezequiel.gambaccini@gmail.com>
2014-08-31 22:30:22 -0300

    Commit inicial tp1

---
+++ tp1/tp1.c
@@ -0,0 +1,5 @@
+#include "stdio.h"
+
+double solve_explicit(double next, double actual, double prev);
+double solve_implicit(double new_next, double new_prev, double next, double actual, double prev);
+
