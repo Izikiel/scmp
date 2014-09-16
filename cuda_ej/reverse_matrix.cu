e2f03f8d58548c16a68ec881a36d8afe639de1e3
Izikiel <ezequiel.gambaccini@gmail.com>
2014-09-16 11:06:36 -0300

    Ahi anda bonito :)

--- cuda_ej/reverse_matrix.cu
+++ cuda_ej/reverse_matrix.cu
@@ -1,6 +1,6 @@
 #include "utils.h"
 
-__global__ void reverse_matrixY(int* matrix, int N)
+__global__ void reverse_matrixY(int *matrix, int N)
 {
     int col = threadIdx.x + blockDim.x * blockIdx.x;
     int row = threadIdx.y + blockDim.y * blockIdx.y;
@@ -12,19 +12,19 @@ __global__ void reverse_matrixY(int* matrix, int N)
     }
 }
 
-__global__ void reverse_matrixX(int* matrix, int N)
+__global__ void reverse_matrixX(int *matrix, int N)
 {
     int col = threadIdx.x + blockDim.x * blockIdx.x;
     int row = threadIdx.y + blockDim.y * blockIdx.y;
 
     if (row < N / 2) {
-        int temp = matrix[row + col * N];
-        matrix[row + col * N] = matrix[N - 1 - row + col * N];
-        matrix[N - 1 - row + col * N] = temp;
+        int temp = matrix[col + row * N];
+        matrix[col + row * N] = matrix[col + (N - 1 - row) * N];
+        matrix[col + (N - 1 - row) * N] = temp;
     }
 }
 
-void print_matrix_msg(const char* msg, int* matrix, int N)
+void print_matrix_msg(const char *msg, int *matrix, int N)
 {
     printf("%s\n", msg);
     for (int i = 0; i < N; ++i) {
@@ -40,11 +40,11 @@ void print_matrix_msg(const char* msg, int* matrix, int N)
 
 }
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
     int N = 10;
 
-    int* h_matrix = (int*) malloc(N * N * sizeof(int));
+    int *h_matrix = (int *) malloc(N * N * sizeof(int));
 
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < N; ++j) {
@@ -54,13 +54,13 @@ int main(int argc, char const* argv[])
 
     print_matrix_msg("Matriz sin reversear\n", h_matrix, N);
 
-    int* d_matrix;
+    int *d_matrix;
     unsigned int blocks = (N / BLOCK_SIZE + (N % BLOCK_SIZE ? 1 : 0));
     dim3 dimGrid(blocks, 1, 1);
     dim3 dimBlockY(MIN(N / 2, BLOCK_SIZE), MIN(N, BLOCK_SIZE), 1);
 
     cudaCheck(
-        cudaMalloc((void**)&d_matrix, N * N * sizeof(int))
+        cudaMalloc((void **)&d_matrix, N * N * sizeof(int))
     );
 
     cudaCheck(
@@ -76,25 +76,25 @@ int main(int argc, char const* argv[])
     print_matrix_msg("Reverseo en Y", h_matrix, N);
 
 
-    // for (int i = 0; i < N; ++i) {
-    //     for (int j = 0; j < N; ++j) {
-    //         h_matrix[i + j * N] = j;
-    //     }
-    // }
-    // print_matrix_msg("Matriz sin reversear\n", h_matrix, N);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            h_matrix[i + j * N] = j;
+        }
+    }
+    print_matrix_msg("Matriz sin reversear\n", h_matrix, N);
 
-    // cudaCheck(
-    //     cudaMemcpy(d_matrix, h_matrix, N * N * sizeof(int), cudaMemcpyHostToDevice)
-    // );
-    // dim3 dimBlockX(MIN(N, BLOCK_SIZE), MIN(N/2, BLOCK_SIZE), 1);
+    cudaCheck(
+        cudaMemcpy(d_matrix, h_matrix, N * N * sizeof(int), cudaMemcpyHostToDevice)
+    );
+    dim3 dimBlockX(MIN(N, BLOCK_SIZE), MIN(N / 2, BLOCK_SIZE), 1);
 
-    // reverse_matrixX <<< dimGrid, dimBlockX >>>(d_matrix, N);
+    reverse_matrixX <<< dimGrid, dimBlockX >>>(d_matrix, N);
 
-    // cudaCheck(
-    //     cudaMemcpy(h_matrix, d_matrix, N * N * sizeof(int), cudaMemcpyDeviceToHost)
-    // );
+    cudaCheck(
+        cudaMemcpy(h_matrix, d_matrix, N * N * sizeof(int), cudaMemcpyDeviceToHost)
+    );
 
-    // print_matrix_msg("Reverseo en X", h_matrix, N);
+    print_matrix_msg("Reverseo en X", h_matrix, N);
 
 
     cudaFree(d_matrix);
