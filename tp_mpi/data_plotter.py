7762104ae714a56648de6cd09aa4c992462bc6ec
Ezequiel Dario Gambaccini <ezequiel.gambaccini@gmail.com>
2014-12-10 17:48:19 -0300

    agrego plotters

---
+++ tp_mpi/data_plotter.py
@@ -0,0 +1,39 @@
+import sys
+
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import pylab
+
+def parse_data(filename):
+    with open(filename, "r") as f:
+        x = []
+        y = []
+        z = []
+        i = 1
+        for l in f:
+            values = map(float, l.strip().split(" "))
+            for pos, val in enumerate(values):
+                x.append(pos)
+                y.append(i)
+                z.append(val)
+            i += 1
+    return (x,y,z)
+
+def plot(filename):
+    x,y,z = parse_data(filename)
+    fig = plt.figure(figsize=(10,10), linewidth=1)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot(x,y,z, "x")
+    t = "Concentration"
+    p = "Position"
+    time = "Time"
+    ax.set_xlabel(p)
+    ax.set_ylabel(time)
+    ax.set_zlabel(t)
+    # plt.show()
+    plt.savefig("out.svg")
+
+
+
+if __name__ == '__main__':
+    plot(sys.argv[1])
---
+++ tp_mpi/plot_steps.py
@@ -0,0 +1,43 @@
+import sys
+
+import matplotlib.pyplot as plt
+
+
+def merge_files(file_a, file_b):
+	with open(file_a, "r") as a:
+		with open(file_b, "r") as b:
+			with open("merge_out.txt", "w") as out:
+				for x, y in zip(a,b):
+					out.write(x.strip() + " " + y)
+
+
+def parse_data(filename):
+	data = []
+	with open(filename, "r") as f:
+		for l in f:
+			data.append(map(float, l.strip().split()))
+	return data
+
+
+def plot_steps(filename):
+	data = parse_data(filename)
+	i = 0.0		
+	fig = plt.figure()
+	for l in data:
+		ax = fig.add_subplot(111)
+		ax.plot(l)
+		ax.axis([0, 20, -1, 2])
+		ax.yaxis.set_label_text("Concentration")
+		ax.xaxis.set_label_text("Position")
+		fig.savefig("step_%f.png"%i)
+		fig.clf()
+		i += 0.1
+
+
+if __name__ == '__main__':
+	if len(sys.argv) == 3:
+		merge_files(sys.argv[1], sys.argv[2])
+		plot_steps("merge_out.txt")
+	elif len(sys.argv) == 2:
+		plot_steps(sys.argv[1])
+
